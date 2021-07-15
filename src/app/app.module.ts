54b43e8372529bad408a48080868b148ded2bc35
Maryna Kutsarenko <m.kutsarenko@gmail.com>
2021-07-15 23:00:44 +0300

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateTodoComponent } from './components/_template/template-todo/template-todo.component';
+import { PageListComponent } from './components/page-list/page-list.component';
+import { TemplateTodoFormComponent } from './components/_template/template-todo-form/template-todo-form.component';
+import { TemplateHeaderComponent } from './components/_template/template-header/template-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateTodoComponent,
+    PageListComponent,
+    TemplateTodoFormComponent,
+    TemplateHeaderComponent
   ],
   imports: [
     BrowserModule,
